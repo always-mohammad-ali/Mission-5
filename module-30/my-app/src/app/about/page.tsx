70bd79c540099aa6046076d6df08d0111dc9d125
always-mohammad-ali <always.mohammad.ali@gmail.com>
2026-01-26 17:22:48 +0600

    added loading section whenever we go from one route to another by using async in that specific route, also handle error

--- module-30/my-app/src/app/about/page.tsx
+++ module-30/my-app/src/app/about/page.tsx
@@ -1,4 +1,11 @@
-export default function AboutPage(){
+import { error } from "console";
+
+
+export default async function AboutPage(){
+    await new Promise((resolve) => setTimeout(resolve, 4000));
+
+    throw new Error("Something went wrong");
+    
     return(
 
         <div>
