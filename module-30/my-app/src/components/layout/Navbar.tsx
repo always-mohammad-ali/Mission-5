040f21cf633c2322609feac202cb6696d96db2e5
always-mohammad-ali <always.mohammad.ali@gmail.com>
2026-01-26 16:44:19 +0600

    implementing toggle mode by installing it from shadcn, then solve hydration error by sufferhydrate something like that by include in html first line, then we add this in navbar

--- module-30/my-app/src/components/layout/Navbar.tsx
+++ module-30/my-app/src/components/layout/Navbar.tsx
@@ -25,6 +25,7 @@ import {
   SheetTrigger,
 } from "@/components/ui/sheet";
 import Link from "next/link";
+import { ModeToggle } from "./ModeToggle";
 
 interface MenuItem {
   title: string;
@@ -107,6 +108,7 @@ const Navbar = ({
             </div>
           </div>
           <div className="flex gap-2">
+            <ModeToggle></ModeToggle>
             <Button asChild variant="outline" size="sm">
               <Link href={auth.login.url}>{auth.login.title}</Link>
             </Button>
@@ -153,8 +155,9 @@ const Navbar = ({
                   >
                     {menu.map((item) => renderMobileMenuItem(item))}
                   </Accordion>
-
+                  
                   <div className="flex flex-col gap-3">
+                    <ModeToggle></ModeToggle>
                     <Button asChild variant="outline">
                       <Link href={auth.login.url}>{auth.login.title}</Link>
                     </Button>
